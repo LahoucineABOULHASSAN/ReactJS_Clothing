1efd08e71b439abf0c7879f893117fa20a15f6cf
LahoucineABOULHASSAN <em.aboulhassan@gmail.com>
2021-05-01 22:29:50 +0100

    add error handling

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,45 +1,68 @@
 import React, { useContext, useEffect, useState } from "react";
 import Product from "./Product.jsx";
+import Error from "../Error.jsx";
 import "../../stylesheets/products.css";
 import { ProductContext } from "../../contexts/ProductContext.jsx";
 import Axios from "axios";
 const Products = () => {
   const { products, setProducts } = useContext(ProductContext);
   const [showAll, setShowAll] = useState(false);
+  const [error, setError] = useState(null);
   useEffect(() => {
     const url =
       "https://raw.githubusercontent.com/LahoucineABOULHASSAN/json_files/main/products.json";
+
     const fetchData = async () => {
-      const res = await Axios.get(url);
-      setProducts(res.data);
+      try {
+        const res = await Axios.get(url);
+        if (res.status !== 200) {
+          throw Error("Couldn't get res");
+        }
+        setProducts(res.data);
+      } catch (err) {
+        const error =
+          err.message === "Network Error"
+            ? "Network Error, Please Check Your Internet Connection"
+            : err.message;
+        setError(error);
+      }
     };
+
     fetchData(); // eslint-disable-next-line
   }, []);
   return (
     <section id="our-products" data-aos="flip-right">
-      <p className="headline flex flex-row">
-        Latest Products
-        <div onClick={() => setShowAll(!showAll)}>
-          {showAll ? (
-            <span>
-              SHOW LESS PRODUCTS<i className="chevron left icon"></i>
-            </span>
-          ) : (
-            <span>
-              SHOW ALL PRODUCTS<i className="chevron right icon"></i>
-            </span>
-          )}
+      {products.length ? (
+        <div>
+          <div className="headline flex flex-row">
+            Latest Products
+            <div onClick={() => setShowAll(!showAll)}>
+              {showAll ? (
+                <span>
+                  SHOW LESS PRODUCTS<i className="chevron left icon"></i>
+                </span>
+              ) : (
+                <span>
+                  SHOW ALL PRODUCTS<i className="chevron right icon"></i>
+                </span>
+              )}
+            </div>
+          </div>
+          <div className="grid">
+            {showAll
+              ? products.map((product) => (
+                  <Product key={product.id} product={product} />
+                ))
+              : products
+                  .slice(0, 8)
+                  .map((product) => (
+                    <Product key={product.id} product={product} />
+                  ))}
+          </div>
         </div>
-      </p>
-      <div className="grid">
-        {showAll
-          ? products.map((product) => (
-              <Product key={product.id} product={product} />
-            ))
-          : products
-              .slice(0, 8)
-              .map((product) => <Product key={product.id} product={product} />)}
-      </div>
+      ) : (
+        <Error error={error} />
+      )}
     </section>
   );
 };
